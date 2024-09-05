5dcf7c24f25df97dc265268ab0e3c0b783f2efce
TheErujeje <ucheohanado@gmail.com>
2024-09-05 18:20:10 +0100

    Vite app setup complete

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function SideNav() {
+  return (<>
+    <div>
+        Hello World
+    </div>
+     {/* <div className="h-screen w-64 bg-gray-800 text-white fixed">
+      <h2 className="text-2xl font-semibold text-center p-4">My App</h2>
+      <ul className="space-y-4">
+        <li className="p-2 hover:bg-gray-700"><Link to="/">Home</Link></li>
+        <li className="p-2 hover:bg-gray-700"><Link to="/articles">Articles</Link></li>
+        <li className="p-2 hover:bg-gray-700"><Link to="/events">Events</Link></li>
+        <li className="p-2 hover:bg-gray-700"><Link to="/careers">Careers</Link></li>
+      </ul>
+    </div> */}
+
+  </> 
+  )
+}
+
+export default SideNav
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideNav from '../components/SideNav.jsx'
+
+function HomePage() {
+  return (<>
+  <div>
+    {/* <SideNav /> */}
+    <h1>Hello World</h1>
+  </div>
+  </>
+  )
+}
+
+export default HomePage
